d57b53bb99f3d897225b232ec56e7e0472c1b0ff
Aditya <75555845+ak-942@users.noreply.github.com>
2024-07-30 18:07:06 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [DisplayName("Employee Name") ]
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
